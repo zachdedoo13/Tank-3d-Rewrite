f5322706df5408d18cdeb577eeea5cb75d2f05d4
zachdedoo13 <140671703+zachdedoo13@users.noreply.github.com>
2024-02-28 18:26:46 +1100

    added clearing of bounceing raycast

--- A Requirements/Preload/RayTool.gd
+++ A Requirements/Preload/RayTool.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+# raycast in direction
 func check_direction(position:Vector3, direction:float, draw=true, collision_mask=2) -> Array:
 	var ret = [false, false, false, false]
 	
@@ -37,8 +37,15 @@ func check_direction(position:Vector3, direction:float, draw=true, collision_mas
 		
 	return ret
 
+func clear_check_direction(ret:Array, on_screen=1) -> Array:
+	if len(ret) > on_screen:
+		ret[0].queue_free()
+		ret.remove_at(0)
+	return ret
+
+
 func check_direction_bounce(position:Vector3, direction:float, draw=true, collision_mask=2) -> Array:
-	var ret = [false, false, false, false]
+	var ret = [false, false, false, false, false, false, false, false]
 	
 	direction += PI
 	
@@ -57,7 +64,13 @@ func check_direction_bounce(position:Vector3, direction:float, draw=true, collis
 	get_tree().get_root().add_child(raycast)
 	raycast.force_raycast_update()
 	
-	ret[0] = Draw3d.line(raycast.position, raycast.get_collision_point())
+	if raycast.is_colliding():
+		ret[0] = true
+		ret[1] = raycast.get_collider()
+		ret[2] = raycast.get_collision_point()
+	
+	if draw:
+		ret[3] = Draw3d.line(raycast.position, raycast.get_collision_point())
 	
 	# bounce
 	var ray3D = raycast.get_collision_normal()
@@ -75,19 +88,18 @@ func check_direction_bounce(position:Vector3, direction:float, draw=true, collis
 	get_tree().get_root().add_child(bounceRaycast)
 	bounceRaycast.force_raycast_update()
 	
-	ret[1] = Draw3d.line(bounceRaycast.position, bounceRaycast.get_collision_point())
+	if bounceRaycast.is_colliding():
+		ret[4] = true
+		ret[5] = bounceRaycast.get_collider()
+		ret[6] = bounceRaycast.get_collision_point()
+	
+	if draw:
+		ret[7] = Draw3d.line(bounceRaycast.position, bounceRaycast.get_collision_point())
 	
 	raycast.queue_free()
 	bounceRaycast.queue_free()
 	
 	return ret
 
-func check_position(position:Vector3, draw=true, collision_mask=2) -> Array:
-	var ret = []
-	return ret
-
-func check_2_positions(pos1:Vector3, pos2:Vector3, draw=true, collision_mask=2) -> Array:
-	var ret = []
-	return ret
 
 
--- entitys/Enemys/TurrentTest.gd
+++ entitys/Enemys/TurrentTest.gd
@@ -1,23 +1,23 @@
 extends Node3D
 
 var debug = []
+var debug2 = []
 
 func _ready():
 	SignalManager.MousePos.connect(look_at_mouse_pos)
 
 
 func _process(delta):
-	pass
-
+	var stuff = RayTool.check_direction_bounce(position, rotation.y, true)
+	debug.append(stuff[3])
+	debug2.append(stuff[7])
+	RayTool.clear_check_direction(debug, 100)
+	RayTool.clear_check_direction(debug2, 100)
+	
 func look_at_mouse_pos(pos):
 	pos.y = position.y
 	look_at(pos)
 	
-	var ret = RayTool.check_direction_bounce(position, rotation.y, true)
 	
-	#debug.append(ret[3])
-	#if len(debug) > 3:
-		#debug[0].queue_free()
-		#debug.remove_at(0)
 
 
