e10985b7fa7d4eb24deeb649079639f173e3598f
zachdedoo13 <140671703+zachdedoo13@users.noreply.github.com>
2024-03-02 21:21:03 +1100

    fixed?

--- A Requirements/Preload/RayTool.gd
+++ A Requirements/Preload/RayTool.gd
@@ -1,7 +1,7 @@
 extends Node
 
 # raycast in direction
-func check_direction(position:Vector3, direction:float, draw=true, collision_mask=2, debug_look_vector=null) -> Array:
+func check_direction(position:Vector3, direction:float, draw=true, collision_masks=[2], debug_look_vector=null) -> Array:
 	var ret = [false, null, false, null, false, false]
 	
 	direction += PI
@@ -12,7 +12,7 @@ func check_direction(position:Vector3, direction:float, draw=true, collision_mas
 		look_vector = debug_look_vector
 	else:
 		look_vector = Vector2(cos(direction), sin(direction))
-	var target = Vector3(look_vector.y, 0, look_vector.x)
+	var target = Vector3(look_vector.y, position.y, look_vector.x)
 	
 	raycast.enabled = true
 	raycast.position = position
@@ -20,7 +20,8 @@ func check_direction(position:Vector3, direction:float, draw=true, collision_mas
 	raycast.target_position.y = position.y
 	raycast.collide_with_areas = true
 	raycast.set_collision_mask_value(1, false)
-	raycast.set_collision_mask_value(collision_mask, true)
+	for i in range(len(collision_masks)):
+		raycast.set_collision_mask_value(collision_masks[i], true)
 	
 	get_tree().get_root().add_child(raycast)
 	raycast.force_raycast_update()
@@ -41,7 +42,7 @@ func check_direction(position:Vector3, direction:float, draw=true, collision_mas
 			line = Draw3d.line(raycast.position, raycast.target_position)
 		ret[3] = line
 		
-		ret[5] = look_vector
+	ret[5] = look_vector
 	return ret
 
 func clear_check_direction(ret:Array, on_screen=1) -> Array:
@@ -49,89 +50,92 @@ func clear_check_direction(ret:Array, on_screen=1) -> Array:
 		if ret[0] != null:
 			ret[0].queue_free()
 		else:
+			print(ret)
+			ret[2938572039578203958] += 1
 			print("saved at queue_free")
 		ret.remove_at(0)
 	return ret
 
-func check_direction_bounce(position:Vector3, direction:float, draw=true, collision_mask=2) -> Array:
-	var ret = [false, false, false, false, false, false, false, false]
-	
-	direction += PI
-	
-	var raycast := RayCast3D.new()
-	var look_vector = Vector2(cos(direction), sin(direction))
-	var target = Vector3(look_vector.y, 0, look_vector.x)
-	
-	#raycast 1
-	raycast.enabled = true
-	raycast.position = position
-	raycast.target_position = target * 1_000
-	raycast.target_position.y = position.y
-	raycast.collide_with_areas = true
-	raycast.set_collision_mask_value(1, false)
-	raycast.set_collision_mask_value(collision_mask, true)
-	
-	get_tree().get_root().add_child(raycast)
-	raycast.force_raycast_update()
-	
-	if raycast.is_colliding():
-		ret[0] = true
-		ret[1] = raycast.get_collider()
-		ret[2] = raycast.get_collision_point()
-	
-	if draw:
-		ret[3] = Draw3d.line(raycast.position, raycast.get_collision_point())
-	
-	# bounce
-	var ray3D = raycast.get_collision_normal()
-	var bouceNormal = Vector2(ray3D.z, ray3D.x)
-	
-	var bounceLookVector = look_vector.bounce(bouceNormal)
-	var bounceRaycast := RayCast3D.new()
-	var bounceTarget = Vector3(bounceLookVector.y, 0, bounceLookVector.x)
-	
-	bounceRaycast.position = raycast.get_collision_point()
-	bounceRaycast.target_position = bounceTarget * 1_000
-	bounceRaycast.collide_with_areas = true
-	raycast.set_collision_mask_value(1, false)
-	raycast.set_collision_mask_value(collision_mask, true)
-	
-	get_tree().get_root().add_child(bounceRaycast)
-	bounceRaycast.force_raycast_update()
-	
-	if bounceRaycast.is_colliding():
-		ret[4] = true
-		ret[5] = bounceRaycast.get_collider()
-		ret[6] = bounceRaycast.get_collision_point()
-	
-	if draw:
-		ret[7] = Draw3d.line(bounceRaycast.position, bounceRaycast.get_collision_point())
-	
-	raycast.queue_free()
-	bounceRaycast.queue_free()
-	
-	
-	return ret
+#func check_direction_bounce(position:Vector3, direction:float, draw=true, collision_mask=2) -> Array:
+	#var ret = [false, false, false, false, false, false, false, false]
+	#
+	#direction += PI
+	#
+	#var raycast := RayCast3D.new()
+	#var look_vector = Vector2(cos(direction), sin(direction))
+	#var target = Vector3(look_vector.y, 0, look_vector.x)
+	#
+	##raycast 1
+	#raycast.enabled = true
+	#raycast.position = position
+	#raycast.target_position = target * 1_000
+	#raycast.target_position.y = position.y
+	#raycast.collide_with_areas = true
+	#raycast.set_collision_mask_value(1, false)
+	#raycast.set_collision_mask_value(collision_mask, true)
+	#
+	#get_tree().get_root().add_child(raycast)
+	#raycast.force_raycast_update()
+	#
+	#if raycast.is_colliding():
+		#ret[0] = true
+		#ret[1] = raycast.get_collider()
+		#ret[2] = raycast.get_collision_point()
+	#
+	#if draw:
+		#ret[3] = Draw3d.line(raycast.position, raycast.get_collision_point())
+	#
+	## bounce
+	#var ray3D = raycast.get_collision_normal()
+	#var bouceNormal = Vector2(ray3D.z, ray3D.x)
+	#
+	#var bounceLookVector = look_vector.bounce(bouceNormal)
+	#var bounceRaycast := RayCast3D.new()
+	#var bounceTarget = Vector3(bounceLookVector.y, 0, bounceLookVector.x)
+	#
+	#bounceRaycast.position = raycast.get_collision_point()
+	#bounceRaycast.target_position = bounceTarget * 1_000
+	#bounceRaycast.collide_with_areas = true
+	#raycast.set_collision_mask_value(1, false)
+	#raycast.set_collision_mask_value(collision_mask, true)
+	#
+	#get_tree().get_root().add_child(bounceRaycast)
+	#bounceRaycast.force_raycast_update()
+	#
+	#if bounceRaycast.is_colliding():
+		#ret[4] = true
+		#ret[5] = bounceRaycast.get_collider()
+		#ret[6] = bounceRaycast.get_collision_point()
+	#
+	#if draw:
+		#ret[7] = Draw3d.line(bounceRaycast.position, bounceRaycast.get_collision_point())
+	#
+	#raycast.queue_free()
+	#bounceRaycast.queue_free()
+	#
+	#
+	#return ret
 
-func new_check_direction_bounce(position:Vector3, direction:float, draw=true, collision_mask=2) -> Array:
+func new_check_direction_bounce(position:Vector3, direction:float, draw=true, collision_masks=2) -> Array:
 	# is collideing | collider | collision point | line1 | line2 |
-	var ret = [false, null, false, null, null]
+	var ret = [false, null, false, Node3D.new(), Node3D.new()]
 	
-	var first_ray = check_direction(position, direction, draw, collision_mask)
+	var first_ray = check_direction(position, direction, draw, collision_masks)
 	
 	# bounce
 	if first_ray[0]:
 		var bouceNormal = Vector2(first_ray[4].z, first_ray[4].x)
 		var bounce_look_vector = first_ray[5].bounce(bouceNormal)
 		
-		var second_ray = check_direction(first_ray[2], direction, draw, collision_mask, bounce_look_vector)
+		
+		var second_ray = check_direction(first_ray[2], 0, draw, collision_masks, bounce_look_vector)
 		
 		if second_ray[0]:
 			ret[0] = true
 			ret[1] = second_ray[1]
 			ret[2] = second_ray[2]
 			ret[3] = second_ray[3]
-	
+		
 	ret[4] = first_ray[3]
 	
 	return ret
--- entitys/Enemys/bacic/Turrent.gd
+++ entitys/Enemys/bacic/Turrent.gd
@@ -6,18 +6,19 @@ var can_shoot = true
 
 
 func look_and_check() -> void:
-	var store = RayTool.new_check_direction_bounce(global_position, rotation.y, true, 5)
+	var store = RayTool.new_check_direction_bounce(global_position, rotation.y, false, [5, 7])
 	if store[0]:
 		if store[1].is_in_group("player"):
 			if can_shoot:
 				shoot()
 				$Timer.start()
 				can_shoot = false
-	line.append(store[3])
-	line2.append(store[4])
-	
-	RayTool.clear_check_direction(line)
-	RayTool.clear_check_direction(line2)
+				
+	#line.append(store[3])
+	#line2.append(store[4])
+	#
+	#RayTool.clear_check_direction(line, 100)
+	#RayTool.clear_check_direction(line2, 100)
 
 func shoot() -> void:
 	SignalManager.SpawnBullet.emit("bacic", "player", $Marker3D.global_position, rotation.y - PI, 30, false)
